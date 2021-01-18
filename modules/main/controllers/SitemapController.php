38a7911a9ebe2ca5c8311a700cbb83775315c300
Sultan Nadirbek <s.nadirbek@cit-orleu.kz>
2021-01-18 14:32:37 +0600

    fixes

--- modules/main/controllers/SitemapController.php
+++ modules/main/controllers/SitemapController.php
@@ -3,7 +3,6 @@
 namespace app\modules\main\controllers;
 
 use app\components\core\Controller;
-use app\components\helpers\Url;
 use Yii;
 use yii\web\Response;
 
@@ -12,6 +11,19 @@ class SitemapController extends Controller
     public function actionIndex()
     {
         $file = Yii::$app->cache->get('cszilla.sitemap');
+
+        if (!$file) {
+            $models = [];
+
+            $host = 'https://cszilla.ru';
+
+            $file = $this->renderPartial('@app/components/templates/sitemap', [
+                'items' => $models,
+                'host' => $host
+            ]);
+
+            Yii::$app->cache->set('cszilla.sitemap', $file, 12*60*60);
+        }
 
         Yii::$app->response->format = Response::FORMAT_RAW;
         Yii::$app->response->headers->add('Content-Type', 'text/xml');
