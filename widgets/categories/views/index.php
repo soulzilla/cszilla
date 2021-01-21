a1126acd94ccad68d56c98c792541a669beaed09
soulzilla <eeza.csgo@gmail.com>
2021-01-21 23:01:53 +0600

    fix

--- widgets/categories/views/index.php
+++ widgets/categories/views/index.php
@@ -13,9 +13,8 @@ if ($category = Yii::$app->request->get('category')) {
 <div class="sb-widget bordered-box mt-sm-3 mt-lg-0">
     <h2 class="sb-title">Разделы</h2>
     <form class="sb-search mb-3" action="<?= $searchUrl ?>" method="get">
-        <label>
-            <input value="<?= Yii::$app->request->get('query') ?>" name="query" type="text" placeholder="Поиск">
-        </label>
+        <label class="d-none" for="search-query">Поиск</label>
+        <input value="<?= Yii::$app->request->get('query') ?>" id="search-query" name="query" type="text" placeholder="Поиск">
     </form>
     <?php if (sizeof($models)): ?>
         <ul class="sb-cata-list">
