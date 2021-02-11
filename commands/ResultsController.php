9831edcd43a0a4b9a3df4d9e7068162973eb4526
soulzilla <eeza.csgo@gmail.com>
2021-02-11 12:15:12 +0600

    faq, contact form, static pages

--- commands/ResultsController.php
+++ commands/ResultsController.php
@@ -5,6 +5,7 @@ namespace app\commands;
 use app\enums\MatchResultEnum;
 use app\models\MatchResult;
 use app\models\Prediction;
+use app\models\PredictionCounter;
 use yii\console\Controller;
 
 class ResultsController extends Controller
@@ -42,6 +43,27 @@ class ResultsController extends Controller
 
             $result->state = MatchResultEnum::STATE_DONE;
             $result->save();
+        }
+    }
+
+    public function actionFix()
+    {
+        $counters = PredictionCounter::find()->all();
+        /** @var PredictionCounter $counter */
+        foreach ($counters as $counter) {
+            $predictions = Prediction::find()->where(['user_id' => $counter->user_id])->all();
+            $counter->predictions = sizeof($predictions);
+            $success = 0;
+            /** @var Prediction $prediction */
+            foreach ($predictions as $prediction) {
+                if ($prediction->is_winner) {
+                    $success += 1;
+                }
+            }
+            $counter->success_predictions = $success;
+            $win_rate = (int) (($success/$counter->predictions) * 100);
+            $counter->win_rate = (string) $win_rate;
+            $counter->save();
         }
     }
 }
